be597c81021bc274162d6ce326ac2e78322a53de
Kinga Aleksiejczuk <kinga.aleksiejczuk28@gmail.com>
2022-11-29 23:35:09 +0100

    Showcase || Preparation

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ProductModel } from '../../models/product.model';
+import { ProductsService } from '../../services/products.service';
+
+@Component({
+  selector: 'app-product',
+  styleUrls: ['./product.component.scss'],
+  templateUrl: './product.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductComponent {
+
+  readonly data$: Observable<ProductModel[]> = this._productsService.getAll();
+
+  constructor(private _activatedRoute: ActivatedRoute, private _productsService: ProductsService) {
+  }
+}
