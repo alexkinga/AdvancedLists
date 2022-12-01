46606abcea8848b81217e25527804853770acf6b
Kinga Aleksiejczuk <kinga.aleksiejczuk28@gmail.com>
2022-12-01 23:03:11 +0100

    Advanced List || Keep selection state

--- src/app/components/filtered-product-list/filtered-product-list.component-module.ts
+++ src/app/components/filtered-product-list/filtered-product-list.component-module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { FlexModule } from '@angular/flex-layout/flex';
 import { FilteredProductListComponent } from './filtered-product-list.component';
 import {RouterLink} from "@angular/router";
 
 @NgModule({
-  imports: [MatCardModule, MatListModule, CommonModule, RouterLink],
+  imports: [MatCardModule, MatListModule, CommonModule, RouterLink, FlexModule],
   declarations: [FilteredProductListComponent],
   providers: [],
   exports: [FilteredProductListComponent]
