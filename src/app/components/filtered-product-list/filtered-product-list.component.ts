1e33d35dac03751e6e8004c5f738ad358d40a4f5
Kinga Aleksiejczuk <kinga.aleksiejczuk28@gmail.com>
2022-12-07 00:50:34 +0100

    Advanced List || Filtering product list || Sorting product list || Exercises for consolidation

--- src/app/components/filtered-product-list/filtered-product-list.component.ts
+++ src/app/components/filtered-product-list/filtered-product-list.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subject, combineLatest } from 'rxjs';
+import {Observable, Subject, combineLatest, BehaviorSubject, of} from 'rxjs';
 import {  map } from 'rxjs/operators';
 import { ProductModel } from '../../models/product.model';
 import { CategoriesService } from '../../services/categories.service';
@@ -16,23 +16,32 @@ export class FilteredProductListComponent {
   readonly categories$: Observable<string[]> = this._categoriesService.getAll();
   private _categorySubject: Subject<string> = new Subject<string>();
   public category$: Observable<string> = this._categorySubject.asObservable();
+
+  private _orderSubject: BehaviorSubject<string> = new BehaviorSubject<string>('asc');
+  public orders: Observable<string[]> = of(['asc', 'desc']);
+  public order$: Observable<string> = this._orderSubject.asObservable();
+
+
   readonly product$: Observable<ProductModel[]> = combineLatest([
     this._productsService.getAll(),
-    this.category$
+    this.category$,
+    this.order$
   ]).pipe(
-    map(([products, category]: [ProductModel[], string]) => {
-      return products.filter(product => product.category === category);
+    map(([products, category, order]: [ProductModel[], string, string]) => {
+      return products.filter(product => product.category === category)
+        .sort((a,b) => {
+          if (a.price > b.price) return order === 'asc' ? 1 : -1;
+          if (a.price < b.price) return order === 'asc' ? -1 : 1;
+          return 0;
+        })
     })
   )
   selectCategory(category: string): void {
     this._categorySubject.next(category);
   }
-  // readonly products$: Observable<ProductModel[]> = combineLatest([
-  //   this._productsService.getAll(),
-  //   this._activatedRoute.params
-  // ]).pipe(map(([products, params]: [ProductModel[], Params]) => {
-  //   return products.filter((product : ProductModel) => product.category === params['category']);
-  // }))
+  sort(order:string){
+    this._orderSubject.next(order);
+  }
 
   constructor(private _categoriesService: CategoriesService, private _productsService: ProductsService, private _activatedRoute: ActivatedRoute) {
   }
